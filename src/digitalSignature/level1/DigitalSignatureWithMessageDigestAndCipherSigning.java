b80f96c47244f2585fd40e898db2284b8119e157
M <manmath738@gmail.com>
2020-08-06 17:08:24 +0530

    digital signature with cipher verifying using methods/key of rsautil

--- src/digitalSignature/level1/DigitalSignatureWithMessageDigestAndCipherSigning.java
+++ src/digitalSignature/level1/DigitalSignatureWithMessageDigestAndCipherSigning.java
@@ -2,6 +2,7 @@ package digitalSignature.level1;
 
 import javax.crypto.Cipher;
 
+import EncryptionStrategies.RSAUtil;
 import digitalSignature.Utils;
 
 import java.nio.file.Files;
@@ -11,18 +12,21 @@ import java.security.PrivateKey;
 
 public class DigitalSignatureWithMessageDigestAndCipherSigning {
 
+	private static String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCgFGVfrY4jQSoZQWWygZ83roKXWD4YeT2x2p41dGkPixe73rT2IW04glagN2vgoZoHuOPqa5and6kAmK2ujmCHu6D1auJhE2tXP+yLkpSiYMQucDKmCsWMnW9XlC5K7OSL77TXXcfvTvyZcjObEz6LIBRzs6+FqpFbUO9SJEfh6wIDAQAB";
+	private static String privateKkey = "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAKAUZV+tjiNBKhlBZbKBnzeugpdYPhh5PbHanjV0aQ+LF7vetPYhbTiCVqA3a+Chmge44+prlqd3qQCYra6OYIe7oPVq4mETa1c/7IuSlKJgxC5wMqYKxYydb1eULkrs5IvvtNddx+9O/JlyM5sTPosgFHOzr4WqkVtQ71IkR+HrAgMBAAECgYAkQLo8kteP0GAyXAcmCAkA2Tql/8wASuTX9ITD4lsws/VqDKO64hMUKyBnJGX/91kkypCDNF5oCsdxZSJgV8owViYWZPnbvEcNqLtqgs7nj1UHuX9S5yYIPGN/mHL6OJJ7sosOd6rqdpg6JRRkAKUV+tmN/7Gh0+GFXM+ug6mgwQJBAO9/+CWpCAVoGxCA+YsTMb82fTOmGYMkZOAfQsvIV2v6DC8eJrSa+c0yCOTa3tirlCkhBfB08f8U2iEPS+Gu3bECQQCrG7O0gYmFL2RX1O+37ovyyHTbst4s4xbLW4jLzbSoimL235lCdIC+fllEEP96wPAiqo6dzmdH8KsGmVozsVRbAkB0ME8AZjp/9Pt8TDXD5LHzo8mlruUdnCBcIo5TMoRG2+3hRe1dHPonNCjgbdZCoyqjsWOiPfnQ2Brigvs7J4xhAkBGRiZUKC92x7QKbqXVgN9xYuq7oIanIM0nz/wq190uq0dh5Qtow7hshC/dSK3kmIEHe8z++tpoLWvQVgM538apAkBoSNfaTkDZhFavuiVl6L8cWCoDcJBItip8wKQhXwHp0O3HLg10OEd14M58ooNfpgt+8D8/8/2OOFaR0HzA+2Dm";
+	
 	public static void main(String[] args) throws Exception {
+		// --------------------------------------------------------------------
+		RSAUtil rsaUtil = new RSAUtil();
 
-		PrivateKey privateKey = Utils.getPrivateKey();
+		// --------------------------------------------------------------------
+		PrivateKey privateKey = rsaUtil.getPrivateKey(privateKkey);
 
-		byte[] messageBytes = Files.readAllBytes(Paths.get("src/test/resources/digitalsignature/message.txt"));
-
-		MessageDigest md = MessageDigest.getInstance("SHA-256");
-		byte[] messageHash = md.digest(messageBytes);
+		byte[] messageBytes = rsaUtil.getHash("manmath".getBytes(), "sha-256").getBytes();
 
 		Cipher cipher = Cipher.getInstance("RSA");
 		cipher.init(Cipher.ENCRYPT_MODE, privateKey);
-		byte[] digitalSignature = cipher.doFinal(messageHash);
+		byte[] digitalSignature = cipher.doFinal(messageBytes);
 
 		Files.write(Paths.get("target/digital_signature_1"), digitalSignature);
 	}
--- src/digitalSignature/level1/DigitalSignatureWithMessageDigestAndCipherVerifying.java
+++ src/digitalSignature/level1/DigitalSignatureWithMessageDigestAndCipherVerifying.java
@@ -2,6 +2,7 @@ package digitalSignature.level1;
 
 import javax.crypto.Cipher;
 
+import EncryptionStrategies.RSAUtil;
 import digitalSignature.Utils;
 
 import java.nio.file.Files;
@@ -11,15 +12,18 @@ import java.security.PublicKey;
 import java.util.Arrays;
 
 public class DigitalSignatureWithMessageDigestAndCipherVerifying {
+	private static String publicKkey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCgFGVfrY4jQSoZQWWygZ83roKXWD4YeT2x2p41dGkPixe73rT2IW04glagN2vgoZoHuOPqa5and6kAmK2ujmCHu6D1auJhE2tXP+yLkpSiYMQucDKmCsWMnW9XlC5K7OSL77TXXcfvTvyZcjObEz6LIBRzs6+FqpFbUO9SJEfh6wIDAQAB";
+	private static String privateKkey = "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAKAUZV+tjiNBKhlBZbKBnzeugpdYPhh5PbHanjV0aQ+LF7vetPYhbTiCVqA3a+Chmge44+prlqd3qQCYra6OYIe7oPVq4mETa1c/7IuSlKJgxC5wMqYKxYydb1eULkrs5IvvtNddx+9O/JlyM5sTPosgFHOzr4WqkVtQ71IkR+HrAgMBAAECgYAkQLo8kteP0GAyXAcmCAkA2Tql/8wASuTX9ITD4lsws/VqDKO64hMUKyBnJGX/91kkypCDNF5oCsdxZSJgV8owViYWZPnbvEcNqLtqgs7nj1UHuX9S5yYIPGN/mHL6OJJ7sosOd6rqdpg6JRRkAKUV+tmN/7Gh0+GFXM+ug6mgwQJBAO9/+CWpCAVoGxCA+YsTMb82fTOmGYMkZOAfQsvIV2v6DC8eJrSa+c0yCOTa3tirlCkhBfB08f8U2iEPS+Gu3bECQQCrG7O0gYmFL2RX1O+37ovyyHTbst4s4xbLW4jLzbSoimL235lCdIC+fllEEP96wPAiqo6dzmdH8KsGmVozsVRbAkB0ME8AZjp/9Pt8TDXD5LHzo8mlruUdnCBcIo5TMoRG2+3hRe1dHPonNCjgbdZCoyqjsWOiPfnQ2Brigvs7J4xhAkBGRiZUKC92x7QKbqXVgN9xYuq7oIanIM0nz/wq190uq0dh5Qtow7hshC/dSK3kmIEHe8z++tpoLWvQVgM538apAkBoSNfaTkDZhFavuiVl6L8cWCoDcJBItip8wKQhXwHp0O3HLg10OEd14M58ooNfpgt+8D8/8/2OOFaR0HzA+2Dm";
 
 	public static void main(String[] args) throws Exception {
+		// --------------------------------------------------------------------
+		RSAUtil rsaUtil = new RSAUtil();
 
-		PublicKey publicKey = Utils.getPublicKey();
+		// --------------------------------------------------------------------
+		PublicKey publicKey = rsaUtil.getPublicKey(publicKkey);
 
-		byte[] messageBytes = Files.readAllBytes(Paths.get("src/test/resources/digitalsignature/message.txt"));
+		byte[] messageBytes = rsaUtil.getHash("manmath".getBytes(), "sha-256").getBytes();
 
-		MessageDigest md = MessageDigest.getInstance("SHA-256");
-		byte[] newMessageHash = md.digest(messageBytes);
 
 		byte[] encryptedMessageHash = Files.readAllBytes(Paths.get("target/digital_signature_1"));
 
@@ -27,7 +31,7 @@ public class DigitalSignatureWithMessageDigestAndCipherVerifying {
 		cipher.init(Cipher.DECRYPT_MODE, publicKey);
 		byte[] decryptedMessageHash = cipher.doFinal(encryptedMessageHash);
 
-		boolean isCorrect = Arrays.equals(decryptedMessageHash, newMessageHash);
+		boolean isCorrect = Arrays.equals(decryptedMessageHash, messageBytes);
 		System.out.println("Signature " + (isCorrect ? "correct" : "incorrect"));
 	}
 
